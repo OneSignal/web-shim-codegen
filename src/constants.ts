9225497da25f330398c646a9cb53968a2f052198
Rodrigo <rogomezpalacio@protonmail.com>
2021-05-21 17:58:33 -0500

    TypingsWriterManager
    Manages creating the typings file to provide package Typescript support
    
    Since we are using the special "initialize" function (only what that defers from WebSDK public API) we need to provide an IFunctionSignature definition.
    
    Includes helper types: types used in OneSignal functions whose interface definitions were not remotely fetched. This can be done in a future version but requires pulling from many remote files, could be fragile if interface is moved.

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { IFunctionSignature } from "./models/FunctionSignature";
+
 export const PATHS = {
   ONESIGNAL_STUBS : '/OneSignal/OneSignal-Website-SDK/master/src/utils/OneSignalStub.ts',
   ONESIGNAL_MAIN  : '/OneSignal/OneSignal-Website-SDK/master/src/OneSignal.ts',
@@ -7,3 +9,26 @@ export const GITHUB_URL = "https://raw.githubusercontent.com";
 
 // OneSignal WebSDK functions we should not add to the package
 export const FUNCTION_IGNORE = ["init"];
+
+export const INITIALIZE_FUNCTION_SIG = {
+  name: "initialize",
+  isAsync: true,
+  arguments: [
+    {
+      name: "appId",
+      type: "string",
+      optional: false
+    },
+    {
+      name: "options",
+      type: "any",
+      optional: true
+    },
+    {
+      name: "events",
+      type: "Array<IOneSignalEvent>",
+      optional: true
+    }
+  ],
+  returnType: "Promise<void>"
+} as IFunctionSignature;
