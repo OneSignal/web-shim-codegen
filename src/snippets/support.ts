78adec47b46a20dbffd1cd69ca2ef3191b168954
Rodrigo <rogomezpalacio@protonmail.com>
2021-06-09 13:04:32 -0500

    Convert Support File to JS, Refactor
    Motivation: for easier maintenance, syntax highlighting, etc... convert from string const to TS file.
    
    - remove unneeded exports
    - add function queueing functionality
    - add new info message regarding initialization attempts

--- src/snippets/support.ts
+++ src/snippets/support.ts
@@ -1,55 +1,32 @@
-export const scriptConstantsTemplate = () => {
-  return `
+// file written in pure JS
 const ONESIGNAL_SDK_ID = 'onesignal-sdk';
 const MODULE_ID = 'onesignal-module';
 const ONE_SIGNAL_SCRIPT_SRC = 'https://cdn.onesignal.com/sdks/OneSignalSDK.js';
-const ONESIGNAL_NOT_SETUP_ERROR = 'OneSignal is not setup correctly.';`;
-};
+const ONESIGNAL_NOT_SETUP_ERROR = 'OneSignal is not setup correctly.';
+const ONESIGNAL_NOT_SETUP_YET = 'OneSignal is not setup yet.'
+const REACT_ONESIGNAL_FUNCTION_QUEUE = [];
 
-export const eventListenersTemplate = () => {
-  return `
 const buildEventListeners = (eventsArr) => {
   let returnStr = '';
 
   if (Array.isArray(eventsArr) && eventsArr.length) {
     eventsArr.forEach((event) => {
       event.listener = event.listener || 'on';
-      returnStr += `+"`OneSignal.${event.listener}('${event.event}', ${event.callback});`;"+`
+      returnStr += `OneSignal.${event.listener}('${event.event}', ${event.callback});`;
     });
   }
   return returnStr;
-};\n`
 };
 
-export const moduleScriptTemplate = () => {
-  return `const getModuleScriptBody = (appId, options = {}, events = []) => {
+const getModuleScriptBody = (appId, options = {}, events = []) => {
     const mappedOptions = JSON.stringify(options, null, 2);
     const listeners = buildEventListeners(events);
 
-    return `+"`"+
-      `var OneSignal = window.OneSignal || [];
-      OneSignal.push(function() {`+
-        "${listeners}"+
-        `OneSignal.init({`+
-          "appId: '${appId}', "+
-          "promptOptions: ${mappedOptions}});});`;\n"+
-  `};\n`
+    return `var OneSignal = window.OneSignal || [];
+      OneSignal.push(function() {${listeners}OneSignal.init({appId: '${appId}', promptOptions: ${mappedOptions}});});`;
 };
 
-export const oneSignalInstanceScriptTemplate = () => {
-  return `export const getOneSignalInstance = () => {
-    const OneSignal = window['OneSignal'];
-
-    if (OneSignal?.initialized) {
-      return OneSignal;
-    }
-
-    return null;
-  };\n`
-};
-
-export const injectScriptTemplate = () => {
-  return `const injectScript = (id, buildScript) => {
+const injectScript = (id, buildScript) => {
   const hasScript = !!document.getElementById(id);
   if (hasScript) {
     return;
@@ -58,30 +35,41 @@ export const injectScriptTemplate = () => {
   script.id = id;
   script = buildScript(script);
   document.body.appendChild(script);
-};\n`
 };
 
-export const injectOneSignalSDKScriptTemplate = () => {
-  return `const injectSDKScript = () => {
+const injectSDKScript = () => {
     injectScript(ONESIGNAL_SDK_ID, (script) => {
       script.src = ONE_SIGNAL_SCRIPT_SRC;
       return script;
     });
-};\n`;
-}
+};
 
-export const injectModuleScriptTemplate = () => {
-  return `const injectModuleScript = (appId, options= {}, events = []) => {
+const injectModuleScript = (appId, options= {}, events = []) => {
   injectScript(MODULE_ID, (script) => {
     script.innerHTML = getModuleScriptBody(appId, options, events);
     script.async = true;
     return script;
   });
-};\n`
+};
+
+const getOneSignalInstance = () => {
+    const OneSignal = window['OneSignal'];
+
+    if (OneSignal?.initialized) {
+      return OneSignal;
+    }
+
+    return null;
+};
+
+const processQueuedOneSignalFunctions = () => {
+  REACT_ONESIGNAL_FUNCTION_QUEUE.forEach(element => {
+    const { name, args } = element;
+    eval(name)(...args);
+  });
 }
 
-export const initOneSignalTemplate = () => {
-  return `const initialize = (appId, options, events = []) => new Promise((resolve, reject) => {
+const initialize = (appId, options, events = []) => new Promise((resolve, reject) => {
   if (!appId) {
     throw new Error('You need to provide your OneSignal appId.');
   }
@@ -94,14 +82,19 @@ export const initOneSignalTemplate = () => {
   let i = 0;
   const interval = setInterval(() => {
     i++;
-    const oneSignal = getOneSignalInstance();
-    if (oneSignal) {
-      clearInterval(interval);
-      resolve();
-    }
     if (i === 10) {
+      clearInterval(interval);
       reject(new Error(ONESIGNAL_NOT_SETUP_ERROR));
     }
-  }, 1000);
-});`
-}
+
+    const oneSignal = getOneSignalInstance();
+
+    if (oneSignal) {
+      clearInterval(interval);
+      processQueuedOneSignalFunctions();
+      resolve();
+    }
+
+    console.info(ONESIGNAL_NOT_SETUP_YET + `(try ${i})`);
+  }, 1_000);
+});
