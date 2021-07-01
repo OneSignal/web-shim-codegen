e97f99206abaacd1e547b901ef07c31a5b22d6be
Rodrigo <rogomezpalacio@protonmail.com>
2021-07-01 16:41:00 -0500

    Moved Function Sig constants into own file + added new ones
    These function signatures are defined here since the function signatures are not fetched remotely OR because (in the case of the init function) are slightly different.
    
    The init in this package will take the appId as the first param and the config object as the second param.

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-import { IFunctionSignature } from "./models/FunctionSignature";
-
 export const PATHS = {
   ONESIGNAL_STUBS : '/OneSignal/OneSignal-Website-SDK/master/src/utils/OneSignalStub.ts',
   ONESIGNAL_MAIN  : '/OneSignal/OneSignal-Website-SDK/master/src/OneSignal.ts',
@@ -9,28 +7,5 @@ export const GITHUB_URL = "https://raw.githubusercontent.com";
 
 // OneSignal WebSDK functions we should not add to the package
 export const FUNCTION_IGNORE = ["init"];
-
-export const INITIALIZE_FUNCTION_SIG = {
-  name: "initialize",
-  isAsync: true,
-  arguments: [
-    {
-      name: "appId",
-      type: "string",
-      optional: false
-    },
-    {
-      name: "options",
-      type: "any",
-      optional: true
-    },
-    {
-      name: "events",
-      type: "Array<IOneSignalEvent>",
-      optional: true
-    }
-  ],
-  returnType: "Promise<void>"
-} as IFunctionSignature;
 
 export const FILE_WRITTEN_IN_PURE_JS = "// file written in pure JS";
---
+++ src/functionSignatures.ts
@@ -0,0 +1,39 @@
+import { IFunctionSignature } from "./models/FunctionSignature";
+
+export const INIT_FUNCTION_SIG = {
+  name: "init",
+  isAsync: true,
+  arguments: [
+    {
+      name: "appId",
+      type: "string",
+      optional: false
+    },
+    {
+      name: "options",
+      type: "any",
+      optional: true
+    },
+  ],
+  returnType: "Promise<void>"
+} as IFunctionSignature;
+
+const EVENTS_FUNCTION_SIG = {
+  isAsync: false,
+  arguments: [
+    {
+      name: "event",
+      type: "string",
+      optional: false
+    },
+    {
+      name: "listener",
+      type: "Function",
+      optional: false
+    }
+  ]
+} as IFunctionSignature;
+
+export const ON_FUNCTION_SIG = { name: "on", ...EVENTS_FUNCTION_SIG };
+export const OFF_FUNCTION_SIG = { name: "off", ...EVENTS_FUNCTION_SIG };
+export const ONCE_FUNCTION_SIG = { name: "once", ...EVENTS_FUNCTION_SIG };
