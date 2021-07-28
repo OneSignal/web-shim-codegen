49c817ec69bac5b240683cff54f54d0515f4c873
Rodrigo <rogomezpalacio@protonmail.com>
2021-07-28 17:12:42 -0500

    VueTypingsWriterManager Implementation
    Writes the OneSignal interface to the index file

---
+++ src/managers/shims/vue/VueTypingsWriterManager.ts
@@ -0,0 +1,11 @@
+import { IFunctionSignature } from "../../../models/FunctionSignature";
+import { TypingsWriterManagerBase } from "../../bases/TypingsWriterManagerBase";
+
+export class VueTypingsWriterManager extends TypingsWriterManagerBase {
+  public writeOneSignalInterface(functions: IFunctionSignature[]): void {
+    this.writeLine(`interface IOneSignal {`);
+    this.writeFunctionTypes(functions, 1);
+    this.writeLine(`\t[index: string]: Function;`);
+    this.writeLine(`}`)
+  }
+}
