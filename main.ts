076f272bff1784782a529f67544fe6681e0afcb7
Rodrigo <rogomezpalacio@protonmail.com>
2021-07-28 17:34:31 -0500

    Update `main.ts` to run both shim code generators

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 import { CodeGenManager } from "./src/managers/CodeGenManager";
+import { Shim } from "./src/models/Shim";
 
-const generator = new CodeGenManager();
-generator.fetchOneSignalFunctions().then(() => {
-  generator.writeIndexFile();
-  generator.writeTypingsFile();
-  generator.writeBuildHelperFiles();
+const reactGenerator = new CodeGenManager(Shim.React);
+reactGenerator.fetchOneSignalFunctions().then(() => {
+  try {
+    reactGenerator.writeIndexFile('js');
+    reactGenerator.writeTypingsFile();
+    reactGenerator.writeBuildHelperFiles();
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+const vueGenerator = new CodeGenManager(Shim.Vue);
+vueGenerator.fetchOneSignalFunctions().then(() => {
+  vueGenerator.writeIndexFile('ts');
+  vueGenerator.writeBuildHelperFiles();
 });
