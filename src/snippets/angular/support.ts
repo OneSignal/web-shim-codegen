1ff6e07090af83d8d2d1183f026561d6a62646f4
Rodrigo <rogomezpalacio@protonmail.com>
2021-09-03 15:26:42 -0500

    Angular Snippets
    These snippets contain the src code that will be written to the service file in the angular scaffolding workspace.

---
+++ src/snippets/angular/support.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+const ONESIGNAL_SDK_ID = 'onesignal-sdk';
+const ONE_SIGNAL_SCRIPT_SRC = 'https://cdn.onesignal.com/sdks/OneSignalSDK.js';
+const ONESIGNAL_NOT_SETUP_ERROR = 'OneSignal is not setup correctly.';
+const MAX_TIMEOUT = 30;
+
+declare global {
+  interface Window {
+    OneSignal: any;
+  }
+}
+
+interface IOneSignalFunctionCall {
+  name: string;
+  args: IArguments;
+  promiseResolver?: Function;
+}
+
+interface IOneSignalService {
+  [key: string]: any
+}
