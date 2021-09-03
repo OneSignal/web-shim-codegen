3a17bfa7ed2a3f56802485c524de11b805260c47
Rodrigo <rogomezpalacio@protonmail.com>
2021-09-03 15:24:16 -0500

    Commit angular-workspace scaffold directory
    Motivation: this directory contains a workspace that builds the angular library

---
+++ src/scaffolds/angular-workspace/projects/onesignal-ngx/src/lib/onesignal-ngx.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'onesignal-onesignal-ngx',
+  template: `
+    <p>
+      onesignal-ngx works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class OnesignalNgxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
