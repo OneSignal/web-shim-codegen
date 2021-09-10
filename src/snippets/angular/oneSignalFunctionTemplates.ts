cd397aa61bc372477352041b292295feb289fa85
Rodrigo <rogomezpalacio@protonmail.com>
2021-09-10 16:37:09 -0500

    Angular OneSignalFunctionTemplate Implementation
    Motivation: these templates are used to generate the OneSignal functions based on their synchronization type

---
+++ src/snippets/angular/oneSignalFunctionTemplates.ts
@@ -0,0 +1,43 @@
+import { IFunctionSignature } from "../../models/FunctionSignature";
+import { spreadArgs, spreadArgsWithTypes } from "../../support/utils";
+
+export const ngOneSignalAsyncFunctionTemplate = (sig: IFunctionSignature) => {
+  const args = sig.arguments?.map(arg => arg.name);
+  return `
+  ${sig.name}(${spreadArgsWithTypes(sig)}): ${sig.returnType || 'void'} {
+    return new Promise((resolve, reject) => {
+      if (!this.doesOneSignalExist()) {
+        this.ngOneSignalFunctionQueue.push({
+          name: "${sig.name}",
+          args: arguments,
+          promiseResolver: resolve,
+        });
+        return;
+      }
+
+      window.OneSignal.push(() => {
+        window.OneSignal.${sig.name}(${spreadArgs(args)})
+          .then((value: ${sig.returnType}) => resolve(value))
+          .catch((error: Error) => reject(error));
+      });
+    });
+  };`
+};
+
+export const ngOneSignalFunctionTemplate = (sig: IFunctionSignature) => {
+  const args = sig.arguments?.map(arg => arg.name);
+  return `
+  ${sig.name}(${spreadArgsWithTypes(sig)}): ${sig.returnType || 'void'} {
+    if (!this.doesOneSignalExist()) {
+      this.ngOneSignalFunctionQueue.push({
+        name: "${sig.name}",
+        args: arguments,
+      });
+      return;
+    }
+
+    window.OneSignal.push(() => {
+      window.OneSignal.${sig.name}(${spreadArgs(args)})
+    });
+  };`
+};
