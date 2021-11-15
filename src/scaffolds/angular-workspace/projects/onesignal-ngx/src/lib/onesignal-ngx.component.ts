6931d9bbbc7c94727494f2bdcef13f19545a0222
Rodrigo <rogomezpalacio@protonmail.com>
2021-11-15 15:50:03 -0600

    fixup! Downgrade angular-workspace to ng 11 for backwards compatibility

--- src/scaffolds/angular-workspace/projects/onesignal-ngx/src/lib/onesignal-ngx.component.ts
+++ src/scaffolds/angular-workspace/projects/onesignal-ngx/src/lib/onesignal-ngx.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-onesignal-ngx',
+  selector: 'onesignal-onesignal-ngx',
   template: `
     <p>
       onesignal-ngx works!
