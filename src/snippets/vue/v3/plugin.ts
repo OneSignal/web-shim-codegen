65061704ca7d53df7b8404212ed043c075b7caa8
Rodrigo <rogomezpalacio@protonmail.com>
2022-04-26 19:44:23 -0500

    Fully support the Composition API
    Motivation: instead of relying on the developer to handle the injection, we can do it for them by providing a wrapper to the inject function so long as we provide the object to the app via `provide`.
    
    Modeled off of https://github.com/vue-gapi/vue-gapi/blob/1e88743991b6d8bf6ad6261fad4996a9914c8c17/src/index.js
    
    Discussion thread: https://github.com/OneSignal/onesignal-vue/issues/4#issuecomment-1107512195

--- src/snippets/vue/v3/plugin.ts
+++ src/snippets/vue/v3/plugin.ts
@@ -1,11 +1,14 @@
+const INJECT_KEY = "onesignal";
+
 export const useOneSignal = () => {
-  return OneSignalVue;
+  return inject(INJECT_KEY);
 }
 
 const OneSignalVuePlugin = {
   install(app: App, options: IInitObject) {
     app.config.globalProperties.$OneSignal = OneSignalVue as IOneSignal;
     app.config.globalProperties.$OneSignal.init(options);
+    app.provide(INJECT_KEY, OneSignalVue);
   }
 }
 
