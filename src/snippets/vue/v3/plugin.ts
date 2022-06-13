2a8379762dcc61ba45a1245856766df4ee9d521f
Rodrigo <rogomezpalacio@protonmail.com>
2022-06-13 18:56:24 -0500

    Modify  function to return  obj instead of use composition API

--- src/snippets/vue/v3/plugin.ts
+++ src/snippets/vue/v3/plugin.ts
@@ -1,14 +1,11 @@
-const INJECT_KEY = "onesignal";
-
 export const useOneSignal = () => {
-  return inject(INJECT_KEY);
+  return OneSignalVue;
 }
 
 const OneSignalVuePlugin = {
   install(app: App, options: IInitObject) {
     app.config.globalProperties.$OneSignal = OneSignalVue as IOneSignal;
     app.config.globalProperties.$OneSignal.init(options);
-    app.provide(INJECT_KEY, OneSignalVue);
   }
 }
 
