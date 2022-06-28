24baa53a093d8943bdec3c7573710edc6d0110ea
Rodrigo <rogomezpalacio@protonmail.com>
2022-06-27 20:27:04 -0500

    Update copy-static script
    Motivation: we need to copy shim-speciic hidden files also

--- scripts/copy-static.sh
+++ scripts/copy-static.sh
@@ -4,6 +4,7 @@ paths=("onesignal-ngx" "react" "vue/v2" "vue/v3")
 
 for tgt in "${paths[@]}"; do
   cp -a src/static/$tgt/* build/$tgt; # shim-specific files
-  rsync -vt src/static/* build/$tgt/; # shared files (e.g: issue templates)
-  rsync -vtr src/static/.[^.]* build/$tgt/; # shared hidden files (e.g: issue templates)
+  rsync -t src/static/* build/$tgt/; # shared files (e.g: issue templates)
+  rsync -tr src/static/$tgt/.[^.]* build/$tgt/; # shim-specific hidden files
+  rsync -tr src/static/.[^.]* build/$tgt/; # shared hidden files (e.g: issue templates)
 done
