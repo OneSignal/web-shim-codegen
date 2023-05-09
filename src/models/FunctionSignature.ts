53eeef40eca0ab87d21cc0ccf4cb36248e513b83
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2023-05-09 17:32:15 -0500

    Update FunctionSignature interface to include geneicTypeParameter
    This will allow us to use define it in the api.json directly.
    
    e.g: 	a value of `<K extends NotificationEventName>` will result in:
    
    addEventListener<K extends NotificationEventName>(event: K, listener: (obj: NotificationEventTypeMap[K]) => void): void;

--- src/models/FunctionSignature.ts
+++ src/models/FunctionSignature.ts
@@ -3,6 +3,7 @@ import { IArgument } from "./Argument";
 export interface IFunctionSignature {
   name: string;
   isAsync: boolean;
+  genericTypeParameter?: string;
   args?: IArgument[];
   returnType?: string;
 }
