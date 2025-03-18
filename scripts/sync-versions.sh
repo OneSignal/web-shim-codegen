8b72ee25d1caf4b4615fe394b48947c310831f6b
Fadi George <fadii925@gmail.com>
2025-03-18 15:26:08 -0700

    add pre commit hook

--- scripts/sync-versions.sh
+++ scripts/sync-versions.sh
@@ -5,10 +5,10 @@ ngxversion=$(npm view onesignal-ngx version)
 reactversion=$(npm view react-onesignal version)
 
 echo "Latest versions:"
-echo "vue2version: $vue2version"
-echo "vue3version: $vue3version"
-echo "ngxversion: $ngxversion"
-echo "reactversion: $reactversion"
+echo "vue: $vue2version"
+echo "vue3: $vue3version"
+echo "angular: $ngxversion"
+echo "react: $reactversion"
 
 echo "Updating local versions"
 npm version --allow-same-version --prefix=src/static/vue/v2 $vue2version --package onesignal-vue
