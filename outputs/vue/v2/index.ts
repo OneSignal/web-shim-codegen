72ec19bc790b1698b5600f14481e602b1e875cb4
sherwinski <sherwin@onesignal.com>
2025-06-05 12:31:03 -0400

    build(vue): opt for silencing ts compiler over wrapping function type in `Promise`

--- outputs/vue/v2/index.ts
+++ outputs/vue/v2/index.ts
@@ -524,11 +524,11 @@ export interface IOneSignalUser {
 	addTags(tags: { [key: string]: string }): void;
 	removeTag(key: string): void;
 	removeTags(keys: string[]): void;
-	getTags(): Promise<{ [key: string]: string }>;
+	getTags(): { [key: string]: string };
 	addEventListener(event: 'change', listener: (change: UserChangeEvent) => void): void;
 	removeEventListener(event: 'change', listener: (change: UserChangeEvent) => void): void;
 	setLanguage(language: string): void;
-	getLanguage(): Promise<string>;
+	getLanguage(): string;
 }
 export interface IOneSignalPushSubscription {
 	id: string | null | undefined;
@@ -889,8 +889,9 @@ function userRemoveTags(keys: string[]): void {
   });
   
 }
-async function userGetTags(): Promise<{ [key: string]: string }> {
-  let retVal: Promise<{ [key: string]: string }>;
+// @ts-expect-error - return non-Promise type despite needing to await OneSignalDeferred
+async function userGetTags(): { [key: string]: string } {
+  let retVal: { [key: string]: string };
   await window.OneSignalDeferred?.push((OneSignal) => {
     retVal = OneSignal.User.getTags();
   });
@@ -917,8 +918,9 @@ function userSetLanguage(language: string): void {
   });
   
 }
-async function userGetLanguage(): Promise<string> {
-  let retVal: Promise<string>;
+// @ts-expect-error - return non-Promise type despite needing to await OneSignalDeferred
+async function userGetLanguage(): string {
+  let retVal: string;
   await window.OneSignalDeferred?.push((OneSignal) => {
     retVal = OneSignal.User.getLanguage();
   });
