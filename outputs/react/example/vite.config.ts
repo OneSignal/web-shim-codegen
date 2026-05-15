819110d8748ab6b71bd5b03f6dc041ba4a288002
Fadi George <fadii925@gmail.com>
2026-05-15 15:07:37 -0700

    feat(examples): add HTTPS via vite-plugin-mkcert

--- outputs/react/example/vite.config.ts
+++ outputs/react/example/vite.config.ts
@@ -1,10 +1,12 @@
 import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
+    https: true, // Not needed for Vite 5+ (simply omit this option)
     port: 3000,
     open: true,
   },
--- outputs/vue/v2/example/vite.config.ts
+++ outputs/vue/v2/example/vite.config.ts
@@ -1,10 +1,12 @@
 import vue from '@vitejs/plugin-vue2';
+import mkcert from 'vite-plugin-mkcert';
 
 import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), mkcert()],
   server: {
+    https: true, // Not needed for Vite 5+ (simply omit this option)
     port: 3000,
     open: true,
   },
