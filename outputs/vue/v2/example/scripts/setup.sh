4df53314718d1f267f4b55f97b7b3f55cb913599
Fadi George <fadii925@gmail.com>
2026-05-15 15:34:06 -0700

    fix(examples): use fixed tarball filename

--- outputs/vue/v2/example/scripts/setup.sh
+++ outputs/vue/v2/example/scripts/setup.sh
@@ -10,16 +10,15 @@ log() {
 
 EXAMPLE_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 PARENT_DIR="$(cd "$EXAMPLE_DIR/.." && pwd)"
+TARBALL="onesignal-vue.tgz"
 
 log "Building onesignal-vue in $PARENT_DIR"
 cd "$PARENT_DIR"
 vp run build
 
 log "Packing onesignal-vue"
-rm -f "$EXAMPLE_DIR"/onesignal-vue-*.tgz
-TARBALL=$(bun pm pack | grep -E '\.tgz$' | tail -1)
-mv "$PARENT_DIR/$TARBALL" "$EXAMPLE_DIR/$TARBALL"
+bun pm pack --filename "$EXAMPLE_DIR/$TARBALL"
 
 log "Installing tarball into example"
 cd "$EXAMPLE_DIR"
-vp add "./$TARBALL"
+vp install --force
