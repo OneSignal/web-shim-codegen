929b3b17f3d4d217536e7710ec077c7afaa28165
Fadi George <fadii925@gmail.com>
2026-05-15 15:16:22 -0700

    fix(examples): remove redundant https option

--- src/static/vue/v3/example/vite.config.ts
+++ src/static/vue/v3/example/vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig } from 'vite-plus';
 export default defineConfig({
   plugins: [vue(), mkcert()],
   server: {
-    https: true, // Not needed for Vite 5+ (simply omit this option)
     port: 3000,
     open: true,
   },
